0165e0c2ae6618cc1bbc2cf72a01d602c9dac9a8
christophezei <christophezeinaty@hotmail.com>
2018-05-03 16:08:04 +0300

    -renamed setName to sharedPreferences

--- app/src/main/java/com/example/christophe/demoapp/activities/MainActivity.java
+++ app/src/main/java/com/example/christophe/demoapp/activities/MainActivity.java
@@ -3,31 +3,24 @@ package com.example.christophe.demoapp.activities;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.TabItem;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.ViewDebug;
 import android.widget.Toast;
-
 import com.example.christophe.demoapp.R;
 import com.example.christophe.demoapp.adapters.PageAdapter;
-import com.example.christophe.demoapp.fragments.tab1Fragment;
-
+import com.example.christophe.demoapp.fragments.Tab1Fragment;
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
-
-public class MainActivity extends AppCompatActivity implements tab1Fragment.ActionListener {
+public class MainActivity extends AppCompatActivity implements Tab1Fragment.ActionListener {
 
     @BindView(R.id.toolbar) Toolbar mtoolbar;
     @BindView(R.id.tablayout) TabLayout tabLayout;
     @BindView(R.id.viewPager) ViewPager viewPager;
-    private SharedPreferences setName;
+    private SharedPreferences sharedPreferences;
     private int count=0;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,21 +34,24 @@ public class MainActivity extends AppCompatActivity implements tab1Fragment.Acti
         viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.setupWithViewPager(viewPager);
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sharedPreferences=getApplication().getSharedPreferences("SaveName", MODE_PRIVATE);
+        String prevName=sharedPreferences.getString("Value","0");
+        if(prevName!="0"){
+            Toast.makeText(this, "Welcome Back you selected:"+prevName+" previously!", Toast.LENGTH_LONG).show();
+        }else
+        {
+            Toast.makeText(this, "Welcome Back ", Toast.LENGTH_LONG).show();
+        }
 
+    }
     @Override
     protected void onPause() {
         super.onPause();
         Toast.makeText(this, "Please don't leave me!", Toast.LENGTH_LONG).show();
     }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setName=getApplication().getSharedPreferences("SaveName", MODE_PRIVATE);
-        String prevName=setName.getString("Value","0");
-        Toast.makeText(this, "Welcome Back you selected:"+prevName+" previously!", Toast.LENGTH_LONG).show();
-    }
-
     @Override
     public void onAction() {
 
